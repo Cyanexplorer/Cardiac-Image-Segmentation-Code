fd93b8b7fe40a94baa0b03bbf015799cd7114e3b
li-hungzhung <explorercyan@gmail.com>
2023-03-05 14:03:49 +0800

    refactor: add missing libs

--- public/target/release/build/log-ca50abc60bd77dee/build_script_build-ca50abc60bd77dee.d
+++
@@ -1,5 +0,0 @@
-C:\Users\cg\Desktop\web_dev\web\public\target\release\build\log-ca50abc60bd77dee\build_script_build-ca50abc60bd77dee.exe: C:\Users\cg\.cargo\registry\src\github.com-1ecc6299db9ec823\log-0.4.14\build.rs
-
-C:\Users\cg\Desktop\web_dev\web\public\target\release\build\log-ca50abc60bd77dee\build_script_build-ca50abc60bd77dee.d: C:\Users\cg\.cargo\registry\src\github.com-1ecc6299db9ec823\log-0.4.14\build.rs
-
-C:\Users\cg\.cargo\registry\src\github.com-1ecc6299db9ec823\log-0.4.14\build.rs:
--- public/target/release/build/wasm-bindgen-shared-29d70410f3be4446/build_script_build-29d70410f3be4446.d
+++
@@ -1,5 +0,0 @@
-C:\Users\cg\Desktop\web_dev\web\public\target\release\build\wasm-bindgen-shared-29d70410f3be4446\build_script_build-29d70410f3be4446.exe: C:\Users\cg\.cargo\registry\src\github.com-1ecc6299db9ec823\wasm-bindgen-shared-0.2.70\build.rs
-
-C:\Users\cg\Desktop\web_dev\web\public\target\release\build\wasm-bindgen-shared-29d70410f3be4446\build_script_build-29d70410f3be4446.d: C:\Users\cg\.cargo\registry\src\github.com-1ecc6299db9ec823\wasm-bindgen-shared-0.2.70\build.rs
-
-C:\Users\cg\.cargo\registry\src\github.com-1ecc6299db9ec823\wasm-bindgen-shared-0.2.70\build.rs:
